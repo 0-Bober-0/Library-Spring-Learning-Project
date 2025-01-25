d4d08fce80c7bbd771795bd7df1b59566832180b
Alexandr Daev <s-daew@ya.ru>
2025-01-26 02:34:43 +0300

    Updating project versions

--- src/main/java/ru/sasha/spring/config/MySpringMvcDispatcherSerlvetIntitializer.java
+++ src/main/java/ru/sasha/spring/config/MySpringMvcDispatcherSerlvetIntitializer.java
@@ -1,10 +1,10 @@
 package ru.sasha.spring.config;
 
 import java.util.EnumSet;
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.FilterRegistration;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
